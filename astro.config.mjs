04b552ac92e77334c8fe8d531a0188acc0898789
KSUM <ksum@KSUMs-MacBook-Air.local>
2026-07-01 12:34:39 +0530

    changes

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import svelte from '@astrojs/svelte';
+
+// import lottie from "astro-integration-lottie";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: true
+    }
+  },
+
+  integrations: [svelte()]
+});
